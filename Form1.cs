e4dc68ad5e8aa310e190b0b1c2fcff98d242929e
Aunt D <dzavizistur@gmail.com>
2025-04-25 13:11:52 +0300

    v1

--- Form1.cs
+++ Form1.cs
@@ -6,10 +6,9 @@ namespace task1
 {
     public partial class Form1 : Form
     {
-        // Глобальные переменные
-        private Point PreviousPoint, point;
+        private Point previousPoint = Point.Empty;
         private Bitmap bmp;
-        private Pen blackPen;
+        private Pen blackPen = new Pen(Color.Black, 4);
         private Graphics g;
 
         public Form1()
@@ -17,105 +16,72 @@ namespace task1
             InitializeComponent();
         }
 
-        // Действия при загрузке формы
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            // Подготавливаем перо для рисования
-            blackPen = new Pen(Color.Black, 4);
-        }
+        private void Form1_Load(object sender, EventArgs e) { }
 
-        // Действия при нажатии кнопки загрузки изображения
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.PNG)|*.bmp;*.jpg;*.gif;*.png";
-            if (dialog.ShowDialog() == DialogResult.OK)
+            using (OpenFileDialog dialog = new OpenFileDialog())
             {
-                Image image = Image.FromFile(dialog.FileName);
-                int width = image.Width;
-                int height = image.Height;
-                pictureBox1.Width = width;
-                pictureBox1.Height = height;
+                dialog.Filter = "Image files (*.BMP, *.JPG, *.PNG)|*.bmp;*.jpg;*.png";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    Image image = Image.FromFile(dialog.FileName);
+                    pictureBox1.Size = new Size(image.Width, image.Height);
 
-                bmp = new Bitmap(image, width, height);
-                pictureBox1.Image = bmp;
-
-                g = Graphics.FromImage(pictureBox1.Image);
+                    bmp = new Bitmap(image);
+                    pictureBox1.Image = bmp;
+                    g = Graphics.FromImage(bmp);
+                }
             }
         }
 
-        // Действия при нажатии мышки в pictureBox1
-        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
-        {
-            PreviousPoint.X = e.X;
-            PreviousPoint.Y = e.Y;
-        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e) => previousPoint = e.Location;
 
-        // Действия при перемещении мышки
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
             {
-                point.X = e.X;
-                point.Y = e.Y;
-                g.DrawLine(blackPen, PreviousPoint, point);
-                PreviousPoint.X = point.X;
-                PreviousPoint.Y = point.Y;
+                g.DrawLine(blackPen, previousPoint, e.Location);
+                previousPoint = e.Location;
                 pictureBox1.Invalidate();
             }
         }
 
-        // Действия при нажатии кнопки сохранения файла
         private void button2_Click(object sender, EventArgs e)
         {
-            SaveFileDialog savedialog = new SaveFileDialog();
-            savedialog.Title = "Сохранить картинку как ...";
-            savedialog.OverwritePrompt = true;
-            savedialog.CheckPathExists = true;
-            savedialog.Filter = "Bitmap File(*.bmp)|*.bmp|GIF File(*.gif)|*.gif|JPEG File(*.jpg)|*.jpg|PNG File(*.png)|*.png";
-            if (savedialog.ShowDialog() == DialogResult.OK)
+            using (SaveFileDialog saveDialog = new SaveFileDialog())
             {
-                string fileName = savedialog.FileName;
-                string strFilExtn = fileName.Remove(0, fileName.Length - 3);
-                switch (strFilExtn)
+                saveDialog.Title = "Сохранить картинку как ...";
+                saveDialog.Filter = "Bitmap File(*.bmp)|*.bmp|JPEG File(*.jpg)|*.jpg|PNG File(*.png)|*.png";
+
+                if (saveDialog.ShowDialog() == DialogResult.OK)
                 {
-                    case "bmp":
-                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
-                        break;
-                    case "jpg":
-                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
-                        break;
-                    case "gif":
-                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif);
-                        break;
-                    case "tif":
-                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Tiff);
-                        break;
-                    case "png":
-                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
-                        break;
-                    default:
-                        break;
+                    string extension = System.IO.Path.GetExtension(saveDialog.FileName).ToLower();
+                    switch (extension)
+                    {
+                        case ".bmp": bmp.Save(saveDialog.FileName, System.Drawing.Imaging.ImageFormat.Bmp); break;
+                        case ".jpg": bmp.Save(saveDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg); break;
+                        case ".png": bmp.Save(saveDialog.FileName, System.Drawing.Imaging.ImageFormat.Png); break;
+                    }
                 }
             }
         }
 
-        // Действия при нажатии кнопки перевода в градации серого
         private void button3_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < bmp.Width; i++)
+            if (bmp != null)
             {
-                for (int j = 0; j < bmp.Height; j++)
+                for (int x = 0; x < bmp.Width; x++)
                 {
-                    int R = bmp.GetPixel(i, j).R;
-                    int G = bmp.GetPixel(i, j).G;
-                    int B = bmp.GetPixel(i, j).B;
-                    int Gray = (R + G + B) / 3;
-                    Color p = Color.FromArgb(255, Gray, Gray, Gray);
-                    bmp.SetPixel(i, j, p);
+                    for (int y = 0; y < bmp.Height; y++)
+                    {
+                        Color pixel = bmp.GetPixel(x, y);
+                        int gray = (pixel.R + pixel.G + pixel.B) / 3;
+                        bmp.SetPixel(x, y, Color.FromArgb(255, gray, gray, gray));
+                    }
                 }
+                pictureBox1.Invalidate();
             }
-            Refresh();
         }
     }
 }
